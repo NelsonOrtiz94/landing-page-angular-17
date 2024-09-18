2a3ad8fbe8cf691b7945fd38e0dd04ac11598d1c
NelsonOrtiz94 <ortnn990@gmail.com>
2024-09-18 12:31:07 -0500

    Se usa interfaz IProduct para que la información de la tabla del componente products pase de ser estática a reactiva

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { IProduct } from '../../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -13,10 +14,14 @@ import { IProduct } from '../../models/product.model';
 export class ProductsComponent implements OnInit {
   productList: IProduct[] = [];
   private _apiService = inject(ApiService);
+  private _router = inject(Router); 
 
   ngOnInit(): void {
     this._apiService.getProducts().subscribe((data: IProduct[]) => {
       this.productList = data;
     });
+  }
+  navegate(id: number): void {
+    this._router.navigate(['/products', id]);
   }
 }
